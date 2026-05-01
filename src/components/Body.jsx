b9ace6e8df6201b028766073b65176922f43dc99
Gowtham <nagamallagowtham@gmail.com>
2026-05-01 15:57:39 +0530

    navigating to login page if user is not loggedIn or authorized

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
+import axios from 'axios'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {withCredentials: true});
+      dispatch(addUser(user.data));
+    } catch(err) {
+      if (err.status === 400 || err.status === 401) {
+        return navigate("/login");
+      }
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
         <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const user = useSelector(store => store.user?.data);
@@ -23,10 +24,10 @@ const Navbar = () => {
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to="/profile" className="justify-between">
                                 Profile
                                 <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
